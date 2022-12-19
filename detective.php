855430281959361c3ed5ef2b43890dc7fe12ec32
Azhar Okapova <azharokapova@mail.ru>
2022-12-19 21:51:39 +0600

    just wanna check something

--- detective.php
+++ detective.php
@@ -19,6 +19,7 @@ include "libs.php"
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
     <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="node_modules\detective.css" />
     <title>JIHC</title>
   </head>
   <body>
